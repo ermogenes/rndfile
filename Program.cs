aa6a3cc5745902e669a745cd1a75fed0012e8f7d
Ermogenes Palacio <ermogenes@users.noreply.github.com>
2022-09-27 11:48:37 -0300

    PoC

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Security.Cryptography;
+
+int mbytes = Convert.ToInt32(args[1]) * 1024 * 1024;
+var bytes = RandomNumberGenerator.GetBytes(mbytes);
+File.WriteAllBytes(args[0], bytes);
